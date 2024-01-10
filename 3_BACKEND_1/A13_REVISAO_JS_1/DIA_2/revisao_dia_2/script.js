7e6092e7ce24b84c5c0ccaa5cd33298c67a8b9f0
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-01-10 17:57:37 -0300

    finalizar base do exs

---
+++ 3_BACKEND_1/A13_REVISAO_JS_1/DIA_2/revisao_dia_2/script.js
@@ -0,0 +1,65 @@
+console.log('Olá turma !')
+
+
+/* WHILE 
+
+- WHILE: Usado quando não sabemos quantas vezes um determinado bloco de instrução prcisa ser repetido.
+
+- Só executa se a condição for verdadeira 
+
+
+while (condição verificada){
+    //comandos que serão executados enquanto ocondição verificada for igual a verdadeiro (true)
+}
+
+*/
+
+
+//------------------  EXERCICIO 1 WHILE --------------------
+
+//------------------  EXERCICIO 2 WHILE --------------------
+
+//------------------  EXERCICIO 3 WHILE --------------------
+
+
+//------------------------------------------------------
+
+/* DO WHILE 
+
+- DO WHILE:Parecido com o while, porém quando utilizamos eles desejamos executar o comando executado ao menos uma vez
+
+
+do{
+
+    //condição ser executado pelo menos uma vez
+
+} while (condicao);
+
+*/
+
+//------------------  EXERCICIO 1 DO WHILE -----------------
+
+//------------------  EXERCICIO 2 DO WHILE -----------------
+
+//------------------------------------------------------
+
+/* FOR 
+
+- FOR: Laço utilizado para repetir uma instrução por um número definido de vezes. 
+
+for (inicialização; condição; expressãoFinal) {
+  // instruções a serem executadas repetidamente
+}
+
+- Inicialização : Expressão executada antes da execução do primeiro laço (geralmente o contador)
+
+- Condição : Verificada a cada interação antes da execução do laço. Se for verdadeira as instruções o trecho de código será executado . 
+
+- Expressão Final : Executado a cada interação do laco (geralmente usado para incrementar o contador)
+
+
+*/
+
+//------------------  EXERCICIO 1 FOR -----------------
+
+//------------------  EXERCICIO 2 FOR -----------------
