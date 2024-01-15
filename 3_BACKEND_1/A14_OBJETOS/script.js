8e8292bbe1b0e89187a3e4460b587aa5e09ba817
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-01-15 19:02:20 -0300

    FAzer base da aula

---
+++ 3_BACKEND_1/A14_OBJETOS/script.js
@@ -0,0 +1,53 @@
+console.log ('Salve Turma!')
+
+//------------ REVISÃO --------------- 
+
+/*
+
+let arrayNumeros = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+for (let i = 0; i < arrayNumeros.length; i++) {
+    console.log(arrayNumeros[i])
+}
+
+
+const arrayNomes = ["João", "Maria", "Pedro", "Ana"]
+
+for (let i = 0; i < arrayNomes.length; i++) {
+    console.log('aqui');
+    console.log(arrayNomes[i]);
+}
+
+*/
+
+
+//------------ OBJETOS ---------------
+
+//inicializando 
+
+// criando um novo objeto
+
+// lendo os dados do objeto
+
+
+//------------ MANIPULAR OBJETOS ---------------
+
+//Inserir uma nova propriedade no objeto 
+
+// Mudar o valor da propriedade 
+
+// Criar um novo objeto baseado em outro
+
+
+//------------ MANIPULAR OBJETOS EM ARRAYS ---------------
+
+//inserir um objeto no array 
+
+// Imprimir itens do array 
+
+
+//----------------------- FOR  -----------------------
+
+//----------------------- FOR OF -----------------------
+
+//----------------------- FOR  EACH  -----------------------
