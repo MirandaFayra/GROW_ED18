0c9c3683c61cd830a2f8e6ce47d80c3605ac05e6
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-01-22 19:27:08 -0300

    Adicionar arquivos da aula

---
+++ 3_BACKEND_1/A16_METODO_ARRAY/script.js
@@ -0,0 +1,24 @@
+//----------- REVISAO -----
+
+//----- ARRAY
+
+//------ OBJETO
+
+//------ FUNCAO 
+
+
+//------ ARROW FUNCTION 
+
+//------ FIND 
+
+//------ FIND INDEX 
+
+//------ FILTER 
+
+//------ MAP
+
+//------ SORT 
+
+//------ SOME 
+
+//------ SPLICE 
