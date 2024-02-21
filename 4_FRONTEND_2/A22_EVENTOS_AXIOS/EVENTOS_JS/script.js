f99551038983e2535fe3be41016e293b34ed8029
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-02-21 18:28:08 -0300

    Finalizar template aula

---
+++ 4_FRONTEND_2/A22_EVENTOS_AXIOS/EVENTOS_JS/script.js
@@ -0,0 +1,54 @@
+
+//-------- ONCLICK ---------
+
+const eventoOnclick =()=>{
+    document.getElementById('meuBotao')
+    alert('Botão clicado!');
+}
+
+
+//-------- ADD EVENT LISTER ---------
+
+const eventoListener =()=>{
+
+    const meuBotao = document.getElementById('botao-escutado')
+
+    function handleClick(event) {
+      console.log('Evento de clique ocorreu!')
+      console.log('Tipo do evento: ', event.type)
+      console.log('Coordenadas do clique: ', event.clientX, event.clientY)
+    }
+
+    meuBotao.addEventListener('click', handleClick)
+}
+
+eventoListener()
+
+//--------------- ON MOUSE HOUVER ------
+
+function mudarCor() {
+    document.getElementById('minha-div').style.background = 'lightblue'
+}
+
+//--------------- ON MOUSE OUT ------
+
+function voltaCor() {
+    document.getElementById('minha-div').style.background = 'transparent'
+}
+
+//--------------- ON KEY UP ------
+
+
+function mostrarTeclaSolta(evento) {
+    document.getElementById('infoTecla').innerText = `Tecla solta: ${evento.key}`;
+}
+
+//--------------- ON KEY DOWN ------
+
+function mostrarTeclaPressionada(evento) {
+    document.getElementById('infoTecla').innerText = `Tecla pressionada: ${evento.key}`;
+}
+
+
+
+
