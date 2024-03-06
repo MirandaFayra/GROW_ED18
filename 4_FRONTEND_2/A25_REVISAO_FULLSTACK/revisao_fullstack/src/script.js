c2f02caefe2a6abb2137253134608e97a5b4a2b2
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-03-06 20:31:16 -0300

    Finalizar endpoint get

--- 4_FRONTEND_2/A25_REVISAO_FULLSTACK/revisao_fullstack/src/script.js
+++ 4_FRONTEND_2/A25_REVISAO_FULLSTACK/revisao_fullstack/src/script.js
@@ -16,6 +16,10 @@ import cors from 'cors'
 
 let PORT = 8080
 
+let listaProdutos = []
+
+//-----
+
 const app = express()
 
 app.use(cors())
@@ -27,12 +31,59 @@ app.use(express.json())
 
 //-------- CRIAR (POST) ------ 
 
+app.post('/criar-produto',(request,response)=>{
+    const nomeProduto = request.body.nomeProduto 
+    const precoProduto = request.body.precoProduto
+
+    try {
+        //O caminho feliz quando a requisicao deu Bom
+        
+        if(!nomeProduto){
+            return response.status(404).send({message:' Nome inválido, informe o nome '})
+        }
+
+        if(!precoProduto){
+            return response.status(404).send({message:' Preço inválido, informe o preço'})
+        }
+
+        const novoProduto ={
+            nomeProduto , 
+            precoProduto
+        }
+
+        listaProdutos.push(novoProduto)
+
+        response.status(201).send({message:`Produto criado com sucesso` ,data:listaProdutos})
+      
+    } catch (error) {
+        // O caminho triste , quando erro 
+        response.status(500).send({message:'Erro interno'})
+    }
+})
+
 
 //--------- LER (GET) --------
+
+app.get('/produtos', (request, response) => {
+    try {
+
+        if(listaProdutos.length>0){
+
+            return response.status(200).json({sucess: true , message: `Produtos retornados com sucesso `, data: listaProdutos})
+
+        }else{
+            response.status(400).send({message:'Lista vazia'})
+        }
+        
+    } catch (error) {
+        response.status(500).send({message:'Erro interno'})
+    }
+})
 
 //-------- ATULIZAR (PUT) ---- 
 
 //------- DELETAR (DELETE) -------
 
+//------- VERIFICAR API  -------
 
 app.listen(PORT, () => console.log("Servidor iniciado na porta 8080"))
